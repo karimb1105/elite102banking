e19ac3d262dfc1b7fb43e18286ce360f4293c287
karimb1105 <80215422+karimb1105@users.noreply.github.com>
2023-05-03 02:34:52 +0000

    completed the interface & menu

--- main.py
+++ main.py
@@ -61,3 +61,56 @@ def modify_account(account_number, pin, name=None, new_pin=None):
     else:
         print('Invalid account number or PIN.')
 
+
+while True:
+    print('Welcome to the bank! Please choose an option:')
+    print('1. Create a new account')
+    print('2. Close an existing account')
+    print('3. Check account balance')
+    print('4. Deposit money')
+    print('5. Withdraw money')
+    print('6. Modify account')
+    print('0. Exit')
+
+    choice = input('Enter your choice: ')
+
+    if choice == '1':
+        create_account()
+
+    elif choice == '2':
+        account_number = input('Enter your account number: ')
+        pin = input('Enter your PIN: ')
+        close_account(account_number, pin)
+
+    elif choice == '3':
+        account_number = input('Enter your account number: ')
+        pin = input('Enter your PIN: ')
+        check_balance(account_number, pin)
+
+    elif choice == '4':
+        account_number = input('Enter your account number: ')
+        pin = input('Enter your PIN: ')
+        amount = float(input('Enter the amount to deposit: '))
+        deposit(account_number, pin, amount)
+
+    elif choice == '5':
+        account_number = input('Enter your account number: ')
+        pin = input('Enter your PIN: ')
+        amount = float(input('Enter the amount to withdraw: '))
+        withdraw(account_number, pin, amount)
+
+    elif choice == '6':
+        account_number = input('Enter your account number: ')
+        pin = input('Enter your PIN: ')
+        name = input('Enter your name (leave blank to skip): ')
+        new_pin = input('Enter a new PIN (leave blank to skip): ')
+        modify_account(account_number, pin, name, new_pin)
+
+    elif choice == '0':
+        print('Thank you for banking with us!')
+        break
+
+    else:
+        print('Invalid choice. Please try again.')
+    
+
